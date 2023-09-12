a2dff13cab7577f45f90a95be9e306f334b5e13e
Jangwoong <cherubkid@gmail.com>
2023-09-12 14:10:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NLog;
+
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
